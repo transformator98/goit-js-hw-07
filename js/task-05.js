cd724872a70468bbbdc59ba10ef689b6679ac860
transformator98 <vanushka16@gmail.com>
2020-08-30 15:11:46 +0300

    replace task-06

--- js/task-05.js
+++ js/task-05.js
@@ -2,6 +2,11 @@ const inputRef = document.querySelector('#name-input');
 const nameRef = document.querySelector('#name-output');
 
 inputRef.addEventListener('change', event => {
-  console.log(event);
   nameRef.textContent = event.target.value;
 });
+inputRef.addEventListener('focus', onInputClear);
+
+function onInputClear() {
+  event.target.value = '';
+  nameRef.textContent = 'незнакомец';
+}
