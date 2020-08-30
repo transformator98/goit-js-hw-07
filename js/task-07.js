70917ee71aa3ed3db79ba219eb1bc3364565cfcb
transformator98 <vanushka16@gmail.com>
2020-08-31 00:17:47 +0300

    done task-07

--- js/task-07.js
+++ js/task-07.js
@@ -1,15 +1,9 @@
 const inputRef = document.querySelector('#font-size-control');
 const textRef = document.querySelector('#text');
 
-inputRef.addEventListener('keydown', event => {
-  //   console.log(event.target.value);
-  //   event.target.value = 0
-  for (let i of event.target.value) {
-    console.log(i);
-    if ((event.target.value[i] += 1)) {
-      console.log('Увеличиваем шрифт');
-    } else {
-      console.log('Уменьшаем шрифт');
-    }
-  }
-});
+function onTextSize() {
+  textRef.style.fontSize = `${inputRef.value}px`;
+}
+
+inputRef.addEventListener('keydown', onTextSize);
+inputRef.addEventListener('mouseup', onTextSize);
