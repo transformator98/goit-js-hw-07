6c4f4093fb92a02799683223f04e3c1efca91f76
transformator98 <vanushka16@gmail.com>
2020-08-30 11:54:57 +0300

    done task-05

---
+++ js/task-05.js
@@ -0,0 +1,6 @@
+const inputRef = document.querySelector('#name-input');
+const nameRef = document.querySelector('#name-output');
+
+inputRef.addEventListener('input', event => {
+  nameRef.textContent = event.target.value;
+});
